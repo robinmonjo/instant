d658b5a2a7029fa2e6ccc71b59ac1acdacf8e1ce
Robin Monjo <robin.monjo@applidget.com>
2013-08-25 00:50:51 +0200

    create base container

--- container/container.go
+++ container/container.go
@@ -5,6 +5,7 @@ import(
   "os/exec"
   "os"
   "log"
+  "io"
 )
 
 const lxc_template = "ubuntu"
@@ -22,14 +23,40 @@ func CreateBaseCn () {
     return
   }
 
+  _, err := exec.LookPath("lxc")
+  if err != nil {
+    log.Fatal("lxc not installed")
+  }
+
   fmt.Println("First usage, creating baseCN ...")
 
-  out, err := exec.Command("/bin/bash", "sudo lxc-create -n " + base_cn_name + " -t " + lxc_template).Output()
+  cmd := exec.Command("sudo", "lxc-create", "-n", base_cn_name, "-t", lxc_template)
+  stdout, err := cmd.StdoutPipe()
   if err != nil {
-    fmt.Println(out)
+    log.Fatal(err)
+  }
+  stderr, err := cmd.StderrPipe()
+  if err != nil {
+    log.Fatal(err)
+  }
+  err = cmd.Start()
+  if err != nil {
     log.Fatal(err)
   }
 
-  fmt.Println(out)
+  io.Copy(os.Stdout, stdout) 
+  io.Copy(os.Stderr, stderr) 
+
+  cmd.Wait()
   fmt.Println("Base container created")
+}
+
+func CreateUserCn () {
+  //create /containers
+  //random id
+  //aufs mount
+  //single ip generate (keep a file on the disk (JSON) that stores cont id, ip, hwaddr)
+  //single hwaddr generate
+  //write config
+  //return a container object (start command + sshCommand + pipe web_sockets to ssh stdin and ssh stdout to websocket)
 }
