5d41f138a180cfd00087f401ff133dfeb07b9014
Robin Monjo <robin.monjo@applidget.com>
2013-08-24 12:52:35 +0200

    encode data to web socket: full communication working !

--- lib/tcp_server.go
+++ lib/tcp_server.go
@@ -39,7 +39,7 @@ func handleConnection(conn net.Conn) {
     
     if err != nil {
       fmt.Println("Error reading:", err.Error())
-      return
+      break
     }
 
     data := buffer[0:n]
@@ -48,13 +48,19 @@ func handleConnection(conn net.Conn) {
       resp := handshakeResp(data)
       _, err = conn.Write([]byte(resp))
       if err != nil {
-        fmt.Println("Failed to write response")
-        return
+        fmt.Println("Failed to write handshake payload")
+        break
       }
       handShaked = true
     } else {
       clearTxt := decode(data)
       fmt.Println(clearTxt)
+
+      _, err = conn.Write([]byte(encode(clearTxt)))
+      if err != nil {
+        fmt.Println("Failed to write response")
+        return
+      }
     }
 
     //reset buffer
@@ -77,16 +83,14 @@ func handshakeResp (rawBytes []byte) string {
     }
   }
 
-  //2: add magick string + sha1 hash
+  //2: add magick string + sha1 hash + base64 enconding
   secretToHash := secret + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
   cypher := sha1.New()
   io.WriteString(cypher, secretToHash)
   hashedSecret := cypher.Sum(nil)
-
-  //3: base64 encode
   encoded := base64.StdEncoding.EncodeToString(hashedSecret)
 
-  //4: clean response string
+  //3: clean response string
   response := "HTTP/1.1 101 Switching Protocols\r\n"
   response += "Upgrade: websocket\r\n"
   response += "Connection: Upgrade\r\n"
@@ -113,6 +117,44 @@ func decode (rawBytes []byte) string {
     decoded[i] = b ^ masks[i % 4]
   }
   return string(decoded)
+}
+
+func encode (message string) (result []byte) {
+  rawBytes := []byte(message)
+  var idxData int
+
+  length := byte(len(rawBytes))
+  if len(rawBytes) <= 125 { //one byte to store data length
+    result = make([]byte, len(rawBytes) + 2)
+    result[1] = length
+    idxData = 2
+  } else if len(rawBytes) >= 126 && len(rawBytes) <= 65535 { //two bytes to store data length
+    result = make([]byte, len(rawBytes) + 4)
+    result[1] = 126 //extra storage needed
+    result[2] = ( length >> 8 ) & 255
+    result[3] = ( length      ) & 255
+    idxData = 4
+  } else {
+    result = make([]byte, len(rawBytes) + 10)
+    result[1] = 127
+    result[2] = ( length >> 56 ) & 255
+    result[3] = ( length >> 48 ) & 255
+    result[4] = ( length >> 40 ) & 255
+    result[5] = ( length >> 32 ) & 255
+    result[6] = ( length >> 24 ) & 255
+    result[7] = ( length >> 16 ) & 255
+    result[8] = ( length >>  8 ) & 255
+    result[9] = ( length       ) & 255
+    idxData = 10
+  }
+
+  result[0] = 129 //only text is supported
+
+  // put raw data at the correct index
+  for i, b := range rawBytes {
+    result[idxData + i] = b
+  }
+  return
 }
 
 
